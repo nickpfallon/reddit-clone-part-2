cfdd1b2749fd1769c8c9b203f21eaf3c1019ad8e
Nick Fallon <nickfallonwastaken@gmail.com>
2017-06-22 17:09:29 -0600

    lskjdf

--- app/app.js
+++ app/app.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
-if (process.env.NODE_ENV !== 'test') {
-  const logger = require('morgan')
-  app.use(logger('dev'))
-}
+// if (process.env.NODE_ENV !== 'test') {
+//   const logger = require('morgan')
+//   app.use(logger('dev'))
+// }
 
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
